6cf1773e5d5c340a0cf2bcc2af60f78b49edc1a5
makotomasui <contact@mathi.jp>
2024-04-02 21:35:50 +0900

    update

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -21,7 +21,16 @@ const GalleryMarquee = ({ media, speed }) => {
 {item.videoid && (
           <>
   <div className={p.video}>
-    <iframe src={`https://player.vimeo.com/video/${item.videoid}?autoplay=1&loop=1&title=0&byline=0&portrait=0&controls=0&mute=1&autopause=0`} width={'100%'} height={'100%'} frameborder="0" allow="autoplay fullscreen" allowfullscreen></iframe>
+              <iframe
+                src={`https://player.vimeo.com/video/${item.videoid}?autoplay=1&loop=1&title=0&byline=0&portrait=0&controls=0&mute=1&autopause=0`}
+                width={'100%'}
+                height={'100%'}
+                frameborder="0"
+                allow="autoplay; fullscreen"
+                allowfullscreen
+                title='vimeo'
+                loading="lazy"
+              />
   </div></>
 )}
       </div>
