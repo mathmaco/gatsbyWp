7dc9f78d8d3ac4bfdc31edd5b23bfdd83787ad5d
makotomasui <contact@mathi.jp>
2024-05-10 12:05:52 +0900

    更新

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -11,7 +11,9 @@ import "./src/css/normalize.css"
 import "./src/css/style.scss"
 
 // gatsby-browser.js
+
 import React from "react";
+import { Helmet } from 'react-helmet';
 import { MarqueeProvider } from './src/contexts/MarqueeContext';
 import { TimeProvider } from "./src/contexts/TimeContext";
 import { ProjectsProvider } from './src/contexts/ProjectsContext';
@@ -26,6 +28,14 @@ export const wrapRootElement = ({ element }) => {
    <ProjectsProvider>
     <TimeProvider>
      <MarqueeProvider>
+      <Helmet>
+       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/yakuhanjp@4.0.1/dist/css/yakuhanjp.css" />
+       <script src="https://webfont.fontplus.jp/accessor/script/fontplus.js?LqAVuNxPvBc%3D&box=z~SmMymcE-s%3D&aa=1&ab=2"></script>
+       <script src="https://webfont.fontplus.jp/accessor/script/fontplus.js?k9wWDE0ZkFQ%3D&box=gykzkMgsKtk%3D&pm=1&aa=1&ab=2"></script>
+       <link rel="preconnect" href="https://fonts.googleapis.com" />
+       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+       <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet" />
+      </Helmet>
       <Header />
       <Projects />
       {element}
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -28,14 +28,6 @@ const Layout = ({ isHomePage, children }) => {
 
   return (
     <>
-      <Helmet>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/yakuhanjp@4.0.1/dist/css/yakuhanjp.css" />
-        <script src="https://webfont.fontplus.jp/accessor/script/fontplus.js?LqAVuNxPvBc%3D&box=z~SmMymcE-s%3D&aa=1&ab=2"></script>
-        <script src="https://webfont.fontplus.jp/accessor/script/fontplus.js?k9wWDE0ZkFQ%3D&box=gykzkMgsKtk%3D&pm=1&aa=1&ab=2"></script>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet" />
-      </Helmet>
         <div className="global-wrapper" data-is-root-path={isHomePage}>
         <main>{children}</main>
         </div>
