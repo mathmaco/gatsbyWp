b02baa378c0f1a0641adb14f38746cb595fa0110
makotomasui <contact@mathi.jp>
2024-05-11 11:11:48 +0900

    調整

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -15,11 +15,6 @@ export const wrapRootElement = ({ element }) => {
    <ProjectsProvider>
     <TimeProvider>
      <MarqueeProvider>
-      <Helmet>
-       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/yakuhanjp@4.0.1/dist/css/yakuhanjp.css" />
-       <script src="https://webfont.fontplus.jp/accessor/script/fontplus.js?LqAVuNxPvBc%3D&box=z~SmMymcE-s%3D&aa=1&ab=2"></script>
-       <script src="https://webfont.fontplus.jp/accessor/script/fontplus.js?k9wWDE0ZkFQ%3D&box=gykzkMgsKtk%3D&pm=1&aa=1&ab=2"></script>
-      </Helmet>
       <Header />
       {element}
       <Projects />
@@ -28,4 +23,12 @@ export const wrapRootElement = ({ element }) => {
    </ProjectsProvider>
   </SelectedValueProvider>
  );
+};
+
+export const onRenderBody = ({ setHeadComponents }) => {
+ setHeadComponents([
+  <link key="yakuhanjp-css" rel="stylesheet" href="https://cdn.jsdelivr.net/npm/yakuhanjp@4.0.1/dist/css/yakuhanjp.css" />,
+  <script key="fontplus-script1" src="https://webfont.fontplus.jp/accessor/script/fontplus.js?LqAVuNxPvBc%3D&box=z~SmMymcE-s%3D&aa=1&ab=2" />,
+  <script key="fontplus-script2" src="https://webfont.fontplus.jp/accessor/script/fontplus.js?k9wWDE0ZkFQ%3D&box=gykzkMgsKtk%3D&pm=1&aa=1&ab=2" />
+ ]);
 };
