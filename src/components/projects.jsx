0441b6160bb9eda92f8ad24e2aed043a4864949a
makotomasui <contact@mathi.jp>
2024-05-11 10:02:05 +0900

    useMemo

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -9,34 +9,36 @@ import Marquee from 'react-fast-marquee';
 import Star from "./star";
  const fillColor = '#c9171e';
 const GalleryMarquee = ({ media, speed, postIndex }) => {
-
-
+  const mediaContent = useMemo(() => {
+    return media.map((item, index) => (
+      <div className={projectStyles.item} key={index}>
+        {item.mediaCheck === 'photo' && item.photo && (
+          <div className={projectStyles.photo}>
+            <GatsbyImage
+              image={item.photo.node.localFile.childImageSharp.gatsbyImageData}
+              style={{ width: '100%', height: '100%' }}
+              alt={item.photo.node.altText || 'デフォルトのサイト名'}
+            />
+          </div>
+        )}
+        {item.mediaCheck === 'video' && item.video && (
+          <div className={projectStyles.video} style={{ paddingTop: item.aspect + '%' }}>
+            <iframe
+              src={`https://player.vimeo.com/video/${item.video}?autoplay=0&loop=1&title=0&byline=0&portrait=0&controls=0&muted=1&autopause=0`}
+              title="vimeo"
+              loading="lazy"
+              frameBorder="0"
+              allow="autoplay;"
+            ></iframe>
+          </div>
+        )}
+      </div>
+    ));
+  }, [media]);
 
   return (
     <Marquee speed={speed} direction={postIndex % 2 === 0 ? 'left' : 'right'} autoFill={true}>
-      {media.map((item, index) => (
-        <div className={projectStyles.item} key={index}>
-          {item.mediaCheck === 'photo' && item.photo && (
-            <div className={projectStyles.photo}>
-              <GatsbyImage
-                image={item.photo.node.localFile.childImageSharp.gatsbyImageData}
-                style={{ width: '100%', height: '100%' }}
-                alt={item.photo.node.altText || 'デフォルトのサイト名'} />
-            </div>
-          )}
-          {item.mediaCheck === 'video' && item.video && (
-            <div className={projectStyles.video} style={{ paddingTop: item.aspect + '%' }}>
-              <iframe
-                src={`https://player.vimeo.com/video/${item.video}?autoplay=0&loop=1&title=0&byline=0&portrait=0&controls=0&muted=1&autopause=0`}
-                title="vimeo"
-                loading="lazy"
-                frameBorder="0"
-                allow="autoplay;"
-              ></iframe>
-            </div>
-          )}
-        </div>
-      ))}
+      {mediaContent}
     </Marquee>
   );
 };
