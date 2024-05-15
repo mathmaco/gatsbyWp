6aed300fa2ae63da94d3a0ef3b8f1d919fb3c4c1
makotomasui <contact@mathi.jp>
2024-05-15 15:13:39 +0900

    projects 動画自動再生

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -26,7 +26,7 @@ const GalleryMarquee = React.memo(({ media, speed, postIndex }) => {
           {item.mediaCheck === 'video' && item.video && (
             <div className={projectStyles.video} style={{ paddingTop: item.aspect + '%' }}>
               <iframe
-                src={`https://player.vimeo.com/video/${item.video}?autoplay=0&loop=1&title=0&byline=0&portrait=0&controls=0&muted=1&autopause=0`}
+                src={`https://player.vimeo.com/video/${item.video}?autoplay=1&loop=1&title=0&byline=0&portrait=0&controls=0&muted=1&autopause=0`}
                 title="vimeo"
                 loading="lazy"
                 frameBorder="0"
