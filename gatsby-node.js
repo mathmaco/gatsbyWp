84f669e757398a15a271b62567d61196436f3327
makotomasui <contact@mathi.jp>
2024-05-15 15:55:56 +0900

    test

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,10 +1,6 @@
 const path = require(`path`)
 const chunk = require(`lodash/chunk`)
 
-// This is a simple debugging tool
-// dd() will prettily dump to the terminal and kill the process
-// const { dd } = require(`dumper.js`)
-
 /**
  * exports.createPages is a built-in Gatsby Node API.
  * It's purpose is to allow you to create pages for your site! 💡
@@ -26,15 +22,13 @@ exports.createPages = async gatsbyUtilities => {
 
   const pages = await getPages(gatsbyUtilities)
 
-  // If there are no posts in WordPress, don't do anything
+  // If there are no pages in WordPress, don't do anything
   if (!pages.length) {
     return
   }
 
-  // If there are posts, create pages for them
+  // If there are pages, create pages for them
   await createIndividualPages({ pages, gatsbyUtilities })
-
-
 }
 
 /**
@@ -42,35 +36,30 @@ exports.createPages = async gatsbyUtilities => {
  */
 const createIndividualBlogPostPages = async ({ posts, gatsbyUtilities }) =>
   Promise.all(
-    posts.map(({ previous, post, next }) =>
-      // createPage is an action passed to createPages
-      // See https://www.gatsbyjs.com/docs/actions#createPage for more info
+    posts.map(({ previous, post, next }) => {
+      // Create the standard blog post page
       gatsbyUtilities.actions.createPage({
-        // Use the WordPress uri as the Gatsby page path
-        // This is a good idea so that internal links and menus work 👍
         path: post.uri,
-
-        // use the blog post template as the page component
         component: path.resolve(`./src/templates/blog-post.js`),
-
-        // `context` is available in the template as a prop and
-        // as a variable in GraphQL.
         context: {
-          // we need to add the post id here
-          // so our blog post template knows which blog post
-          // the current page is (when you open it in a browser)
           id: post.id,
-          isPreview: true, // プレビュー用のフラグを追加
-
-
-          // We also use the next and previous id's to query them and add links!
+          isPreview: false,
           previousPostId: previous ? previous.id : null,
           nextPostId: next ? next.id : null,
         },
       })
-    )
-  )
 
+      // Create the preview blog post page
+      gatsbyUtilities.actions.createPage({
+        path: `/gatsby-source-wordpress/${post.id}`,
+        component: path.resolve(`./src/templates/preview-post.js`),
+        context: {
+          id: post.id,
+          isPreview: true,
+        },
+      })
+    })
+  )
 
 async function getPosts({ graphql, reporter }) {
   const graphqlResult = await graphql(/* GraphQL */ `
@@ -81,14 +70,10 @@ async function getPosts({ graphql, reporter }) {
           previous {
             id
           }
-
-          # note: this is a GraphQL alias. It renames "node" to "post" for this query
-          # We're doing this because this "node" is a post! It makes our code more readable further down the line.
           post: node {
             id
             uri
           }
-
           next {
             id
           }
@@ -108,16 +93,12 @@ async function getPosts({ graphql, reporter }) {
   return graphqlResult.data.allWpPost.edges
 }
 
-
-/**
- * This function creates all the individual blog pages in this site
- */
 const createIndividualPages = async ({ pages, gatsbyUtilities }) =>
   Promise.all(
     pages.map(({ node }) => {
       const componentPath = node.uri === '/about/' ?
-        './src/templates/about-page.js' : // aboutページ専用のテンプレート
-        './src/templates/page.js';        // その他の一般的なページ用のテンプレート
+        './src/templates/about-page.js' :
+        './src/templates/page.js';
 
       return gatsbyUtilities.actions.createPage({
         path: node.uri,
@@ -125,14 +106,13 @@ const createIndividualPages = async ({ pages, gatsbyUtilities }) =>
         context: {
           id: node.id,
         },
-      });
+      })
     })
-  );
+  )
 
 async function getPages({ graphql, reporter }) {
   const graphqlResultPages = await graphql(/* GraphQL */ `
     query WpPages {
-      # Query all WordPress blog posts sorted by date
       allWpPage(sort: { fields: [date], order: DESC }) {
         nodes {
           featuredImage {
---
+++ src/templates/preview-post.js
@@ -0,0 +1,259 @@
+import React, { useState, useEffect } from "react"
+import { Link, graphql, navigate } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
+import parse from 'html-react-parser'
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import Scrollbar from '../components/scrollbar'
+import IconClose from "../components/icon_close"
+import * as single from '../css/components/project-single.module.scss'
+import Star from "../components/star";
+import IconArrow from "../components/icon_arrow";
+
+const BlogPostTemplate = ({ data, location, pageContext }) => {
+ const [post, setPost] = useState(data.post);
+ const isPreview = data.isPreview || false // プレビュー用のフラグを追加
+
+ useEffect(() => {
+  setPost(data.post);
+ }, [data.post]);
+
+
+ const handleModalClose = () => {
+  // Check if it's the first page
+  if (location.pathname === '/') {
+   navigate('/');
+  } else {
+   window.history.back();
+  }
+ };
+ //const featuredImage = post.featuredImage?.node?.localFile?.childImageSharp?.gatsbyImageData
+ const category = post.categories.nodes
+ const tag = post.tags.nodes
+ //const speed = post.projects.projectsGallerySpeed
+ //const mediaCheck = post.projects.projectsMedia.mediaCheck
+ const media = post.projects.projectsMedia
+ const credit = post.projects.projectsCredit
+ const count = post.projects.projectsMediaCount
+ const power = post.projects.projectsMediaPower
+ //const subTtlEn = post.projects.projectsSubtitleEn
+ const subTtlJa = post.projects.projectsSubtitleJa
+ //const ttlEn = post.projects.projectsTitleEn
+ const url = post.projects.projectsUrl
+
+ const fillColor = "#000";
+
+ const options = {
+  replace: ({ attribs, children, name }) => {
+   if (!attribs) {
+    return;
+   }
+
+   if (name === 'br') {
+    return <br />;
+   }
+  }
+ };
+
+
+ return (
+  <Layout location={location}>
+   <Seo
+    pagetitle={post.title}
+    pagedesc={post.excerpt}
+   />
+   <div className="modal">
+    <div className="modal-inner">
+     <Scrollbar>
+      <article
+       className={single.post}
+       itemScope
+       itemType="http://schema.org/Article"
+      >
+       <div className={single.postCont}>
+        <div className={single.postContLeft}>
+         <div className={single.mediaList}>
+          <ul>
+           {media.map((item, index) => (
+            <li key={index}>
+             {item.mediaCheck === 'photo' && item.photo && (
+              <div className={single.photo}>
+               <GatsbyImage
+                image={item.photo.node.localFile.childImageSharp.gatsbyImageData}
+                style={{ width: '100%' }}
+                alt={item.altText || "画像名"}
+               />
+              </div>
+             )}
+             {item.mediaCheck === 'video' && item.video && (
+              <div className={single.video} style={{ paddingTop: item.aspect + '%' }}>
+               <iframe
+                src={`https://player.vimeo.com/video/${item.video}?autoplay=0&loop=0&title=0&byline=0&portrait=0&controls=1&muted=0&autopause=0`}
+                frameBorder="0"
+                title="vimeo"
+                loading="lazy"
+               />
+              </div>
+             )}
+            </li>
+           ))}
+          </ul>
+         </div>
+        </div>
+        <div className={single.postContRight}>
+         <header className={single.header}>
+          <div className={single.TtlCont}>
+           <h1 itemProp="headline" className={single.ttlja}>{parse(post.title)}</h1>
+           {isPreview && <p>This is a preview</p>}
+           <div className={single.subttlja}>{subTtlJa}</div>
+          </div>
+          <div className={single.meta}>
+           <div className={single.metaBox}>
+            <div className={single.metaBoxLeft}>
+             {category && (
+              <ul className={single.categoryList}>
+               {category.map((cat, index) => (
+                <li key={index}>{cat.name}</li>
+               ))}
+              </ul>
+             )}
+             {tag && (
+              <ul className={single.tagList}>
+               {tag.map((tags, index) => (
+                <li key={index}>{tags.name}</li>
+               ))}
+              </ul>
+             )}
+             <div className={single.date}>{post.date}</div>
+            </div>
+            <div className={single.metaBoxRight}>
+             <div className={single.power}>P{power}</div>
+             <div className={single.count}>[{count}]</div>
+             <div className={single.star}><i className="icon-star"><Star fill={fillColor} w={10} h={10} /></i></div>
+            </div>
+           </div>
+          </div>
+
+          {/*{featuredImage && (
+                      <GatsbyImage
+                        image={featuredImage}
+                        alt={post.featuredImage.node.altText}
+                        style={{ marginBottom: 50 }}
+                      />
+                    )}*/}
+         </header>
+
+
+         {!!post.content && (
+          <section itemProp="articleBody" className={single.textarea}>
+           {parse(post.content, options)}
+          </section>
+         )}
+
+         {!!credit && (
+          <footer className={single.footer}>
+           <div className={single.credit}>{parse(credit)}</div>
+           <div className={single.url}><a href={url} target="_blank" rel="noreferrer">{url}<i className={single.linkArrow}><IconArrow fill={fillColor} /></i></a></div>
+          </footer>
+         )}
+
+        </div>
+       </div>
+      </article>
+     </Scrollbar>
+     {
+      location.pathname === '/' ? (
+       <div
+        role="button"
+        tabIndex={0}
+        className="modal-close"
+        onClick={handleModalClose}
+        onKeyDown={(e) => {
+         if (e.key === 'Enter' || e.key === ' ') {
+          handleModalClose();
+         }
+        }}
+        aria-label="Close modal"
+       >
+        <IconClose />
+       </div>
+      ) : (
+       <Link to="/" className="modal-close">
+        <IconClose />
+       </Link>
+      )
+     }
+    </div>
+   </div>
+  </Layout>
+ )
+}
+
+export default BlogPostTemplate
+
+export const pageQuery = graphql`
+  query BlogPostById(
+    $id: String!
+  ) {
+    post: wpPost(id: {eq: $id}) {
+      id
+      excerpt
+      content
+      title
+      categories {
+        nodes {
+          name
+          slug
+          id
+        }
+      }
+      tags {
+        nodes {
+          name
+          id
+          slug
+        }
+      }
+      projects {
+        projectsGallerySpeed
+        projectsCredit
+        projectsMediaCount
+        projectsMediaPower
+        projectsSubtitleEn
+        projectsSubtitleJa
+        projectsTitleEn
+        projectsUrl
+        projectsMedia {
+          mediaCheck
+          video
+          aspect
+          photo {
+            node {
+              altText
+              localFile {
+                childImageSharp {
+                  gatsbyImageData(placeholder: DOMINANT_COLOR, quality: 100, layout: FULL_WIDTH)
+                }
+              }
+            }
+          }
+        }
+      }
+      date(formatString: "MMMM YYYY")
+      featuredImage {
+        node {
+          altText
+          localFile {
+            childImageSharp {
+              gatsbyImageData(
+                quality: 100
+                placeholder: TRACED_SVG
+                layout: FULL_WIDTH
+              )
+            }
+          }
+        }
+      }
+    }
+  }
+`
