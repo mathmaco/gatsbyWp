1b89bfc5dd474109d3ca82521065ade12afb34a3
makotomasui <contact@mathi.jp>
2024-07-01 21:44:21 +0900

    アップデート

--- src/components/LazyVideo.js
+++ src/components/LazyVideo.js
@@ -7,6 +7,7 @@ import PixelPhoto from './PixelPhoto';
 const LazyVideo = ({ videoUrl, thumbnailUrl, width, height, aspectRatio }) => {
  const [intersectionRef, isVisible] = useIntersectionObserver(0);
  const [hasLoaded, setHasLoaded] = useState(false);
+ const [hasPixelated, setHasPixelated] = useState(false); // ピクセル処理が実行されたかどうかを追跡
  const playerRef = useRef(null);
 
  useEffect(() => {
@@ -15,14 +16,39 @@ const LazyVideo = ({ videoUrl, thumbnailUrl, width, height, aspectRatio }) => {
   }
  }, [isVisible, hasLoaded]);
 
+ useEffect(() => {
+  if (playerRef.current) {
+   if (isVisible) {
+    playerRef.current.getInternalPlayer().play();
+   } else {
+    playerRef.current.getInternalPlayer().pause();
+   }
+  }
+ }, [isVisible]);
+
+ const handleVideoReady = () => {
+  setHasLoaded(true);
+  setHasPixelated(true); // ピクセル処理が実行されたことを記録
+ };
+
  return (
   <div style={{ width: '100%', height: '100%', position: 'relative' }} className={projectStyles.media}>
    <div className={projectStyles.video} style={{ aspectRatio }} ref={intersectionRef}>
-    {!hasLoaded && (
+    {!hasPixelated && !hasLoaded && (
      <PixelPhoto
       src={thumbnailUrl}
       width={width}
       height={height}
+     />
+    )}
+    {hasLoaded && !isVisible && (
+     <img
+      src={thumbnailUrl}
+      width="100%"
+      height="100%"
+      alt="Thumbnail"
+      style={{ position: 'absolute', top: 0, left: 0 }}
+      className={projectStyles.thumbnail}
      />
     )}
     {hasLoaded && (
@@ -35,7 +61,8 @@ const LazyVideo = ({ videoUrl, thumbnailUrl, width, height, aspectRatio }) => {
       muted={true}
       width="100%"
       height="100%"
-      style={!isVisible ? { display: 'none' } : {}}
+      onReady={handleVideoReady}
+      style={isVisible ? {} : { display: 'none' }}
      />
     )}
    </div>
