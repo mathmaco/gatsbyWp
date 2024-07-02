377df38c2d0f1e199265f5c5a4b82f32f91c4e38
makotomasui <contact@mathi.jp>
2024-07-02 12:15:49 +0900

    ロードをビューポートのみ

--- src/components/LazyVideo.js
+++ src/components/LazyVideo.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import ReactPlayer from 'react-player';
 import * as projectStyles from '../css/components/project.module.scss';
 import useIntersectionObserver from './useIntersectionObserver';
@@ -7,60 +7,35 @@ import PixelPhoto from './PixelPhoto';
 const LazyVideo = ({ videoUrl, thumbnailUrl, width, height, aspectRatio }) => {
  const [intersectionRef, isVisible] = useIntersectionObserver(0);
  const [hasLoaded, setHasLoaded] = useState(false);
- const [hasPixelated, setHasPixelated] = useState(false); // ピクセル処理が実行されたかどうかを追跡
- const [showThumbnail, setShowThumbnail] = useState(true); // サムネイルの表示制御
- const playerRef = useRef(null);
 
  useEffect(() => {
-  if (isVisible && !hasPixelated) {
-   setHasPixelated(true);
-  }
- }, [isVisible, hasPixelated]);
-
- useEffect(() => {
-  if (isVisible) {
-   setShowThumbnail(false);
-  } else if (hasLoaded) {
-   setShowThumbnail(true);
-   if (playerRef.current) {
-    playerRef.current.seekTo(0); // ビデオを最初に戻す
-   }
+  if (isVisible && !hasLoaded) {
+   setHasLoaded(false); // ビデオがビューポート内に入ったら再度読み込み待機状態にする
   }
  }, [isVisible, hasLoaded]);
-
- const handleVideoReady = () => {
-  setHasLoaded(true);
- };
 
  return (
   <div style={{ width: '100%', height: '100%', position: 'relative' }} className={projectStyles.media}>
    <div className={projectStyles.video} style={{ aspectRatio }} ref={intersectionRef}>
-    {!hasPixelated && !hasLoaded && (
+    {!hasLoaded && (
      <PixelPhoto
       src={thumbnailUrl}
       width={width}
       height={height}
      />
     )}
-    {hasPixelated && (
-     <>
-      <img
-       src={thumbnailUrl}
-       className={`${projectStyles.thumbnail} ${!showThumbnail ? projectStyles.hidden : ''}`}
-       alt="Thumbnail"
-      />
-      <ReactPlayer
-       ref={playerRef}
-       url={videoUrl}
-       playing={isVisible}
-       loop={true}
-       controls={false}
-       muted={true}
-       width="100%"
-       height="100%"
-       onReady={handleVideoReady}
-      />
-     </>
+    {isVisible && (
+     <ReactPlayer
+      url={videoUrl}
+      playing={true}
+      loop={true}
+      controls={false}
+      muted={true}
+      width="100%"
+      height="100%"
+      onReady={() => setHasLoaded(true)}
+      style={!hasLoaded ? { display: 'none' } : {}}
+     />
     )}
    </div>
   </div>
